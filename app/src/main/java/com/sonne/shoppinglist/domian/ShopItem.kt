31870f84e77f514f235e638fde6d937cbc4a5e92
sonne <sonnne34@gmail.com>
2022-01-28 13:55:52 +0500

    Data completed

--- app/src/main/java/com/sonne/shoppinglist/domian/ShopItem.kt
+++ app/src/main/java/com/sonne/shoppinglist/domian/ShopItem.kt
@@ -1,10 +1,16 @@
 package com.sonne.shoppinglist.domian
 
 data class ShopItem(
-    val id: Int,
+//    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    //  пока нет реализации БД, использую данный id
+    var id: Int = UNDEFINED_ID
+) {
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+}
 
 
